6e44e35e5a559895c38ffdc7f43160d60a981453
Calle Englund <calle@discord.bofh.se>
2018-12-01 14:48:37 +0100

    Implement vector cross product

--- lib/glistening_ruby/tuple.rb
+++ lib/glistening_ruby/tuple.rb
@@ -58,6 +58,16 @@ module GlisteningRuby
       @x * other.x + @y * other.y + @z * other.z + @w * other.w
     end
 
+    def cross(other) # rubocop:disable Metrics/AbcSize
+      raise TypeError, 'Only valid for Vectors' unless vector?
+
+      Vector[
+        @y * other.z - @z * other.y,
+        @z * other.x - @x * other.z,
+        @x * other.y - @y * other.x
+      ]
+    end
+
     def magnitude
       (@x * @x + @y * @y + @z * @z + @w * @w)**0.5
     end
