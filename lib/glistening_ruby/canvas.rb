017d1b50938f4f5798ac4db9a6f1b4d299d4c8d8
Calle Englund <calle@discord.bofh.se>
2018-12-01 16:39:49 +0100

    Implement basic drawable canvas

---
+++ lib/glistening_ruby/canvas.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative 'color'
+
+module GlisteningRuby
+  # A drawable canvas
+  class Canvas
+    def self.[](*args)
+      new(*args)
+    end
+
+    def initialize(width, height)
+      @w = width
+      @h = height
+      @pixels = Array.new(width * height, Color[0, 0, 0])
+    end
+
+    attr_reader :w, :h
+
+    def [](x_pos, y_pos)
+      @pixels[x_pos + y_pos * @w]
+    end
+
+    def []=(x_pos, y_pos, color)
+      @pixels[x_pos + y_pos * @w] = color
+    end
+
+    def each
+      x = y = 0
+      @pixels.each do |pixel|
+        yield pixel, x, y
+        x += 1
+        if x == @w
+          y += 1
+          x = 0
+        end
+      end
+    end
+  end
+end
