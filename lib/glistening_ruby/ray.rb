28f7e357f8041ec28e786b3ba56daf5330c449f0
Calle Englund <calle@discord.bofh.se>
2018-12-03 10:23:25 +0100

    Begin the implementation of rays

---
+++ lib/glistening_ruby/ray.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module GlisteningRuby
+  # A ray to be cast into the world
+  class Ray
+    def self.[](*args)
+      new(*args)
+    end
+
+    def initialize(origin, direction)
+      @origin = origin
+      @direction = direction
+    end
+
+    def to_s
+      "#<#{self.class}: ·#{origin} →#{direction}>"
+    end
+    alias inspect to_s
+
+    attr_reader :direction, :origin
+
+    def position(time)
+      @origin + @direction * time
+    end
+  end
+end
