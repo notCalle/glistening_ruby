b8c3b8e1a8b36d55a3796a2b9f4f0068f480469a
Calle Englund <calle@discord.bofh.se>
2018-12-07 09:51:11 +0100

    Refactor intersections to be properly sorted

--- lib/glistening_ruby/intersection.rb
+++ lib/glistening_ruby/intersection.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+require_relative 'computations'
+
 module GlisteningRuby
   # An intersection between a ray and something
   class Intersection
     include Comparable
+    extend Forwardable
 
     def self.[](*args)
       new(*args)
@@ -16,12 +20,11 @@ module GlisteningRuby
 
     attr_reader :object, :t
 
-    # Negative intersections are further away than any positive intersection
-    def <=>(other)
-      return 1 if t.negative?
-      return -1 if other.t.negative?
 
-      t.<=>(other.t)
+    def <=>(other)
+      @t.<=>other.t
     end
+
+    def_delegators :@t, :positive?, :negative?, :zero?
   end
 end
--- lib/glistening_ruby/intersections.rb
+++ lib/glistening_ruby/intersections.rb
@@ -1,22 +1,36 @@
 # frozen_string_literal: true
 
-require 'delegate'
+require 'forwardable'
 require_relative 'intersection'
 
 module GlisteningRuby
   # A set of intersections
-  class Intersections < SimpleDelegator
+  class Intersections
+    extend Forwardable
+    include Enumerable
+
     def self.[](*args)
       new(*args)
     end
 
     def initialize(*intersections)
-      super(intersections)
+      @intersections = []
+      @hit = nil
+      self << intersections
     end
 
-    def hit
-      result = min
-      return result unless result&.t&.negative?
+    def <<(intersections)
+      intersections.each do |intersection|
+        if intersection.positive?
+          @hit = intersection unless @hit && @hit < intersection
+        end
+        @intersections << intersection
+      end
+      @intersections.sort!
     end
+
+    attr_reader :hit
+
+    def_delegators :@intersections, :each, :[]
   end
 end
