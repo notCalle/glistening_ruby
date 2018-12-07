0bdc32df08183b4d3629831c512b0acc3dd88925
Calle Englund <calle@discord.bofh.se>
2018-12-07 09:49:40 +0100

    Add world object

--- lib/glistening_ruby/world.rb
+++ lib/glistening_ruby/world.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
-require 'glistening_ruby'
+require_relative 'base'
+require_relative 'color'
+require_relative 'intersections'
+require_relative 'point'
+require_relative 'point_light'
+require_relative 'sphere'
 
 module GlisteningRuby
-  # World
-  class World
-    def self.[](*args)
-      new(*args)
+  # The whole world
+  class World < Base
+    def initialize
+      @objects = []
+      @lights = []
+      super
     end
+
+    attr_accessor :lights, :objects
+
+    def intersect(ray)
+      @objects.each.with_object(Intersections.new) do |object, intersections|
+        intersections << object.intersect(ray)
+      end
+    end
+  end
+
+  World::DEFAULT = World.new do |w|
+    w.objects << Sphere.new do |s|
+      s.material.color = [0.8, 1.0, 0.6]
+      s.material.diffuse = 0.7
+      s.material.specular = 0.2
+    end
+    w.objects << Sphere.new { |s| s.transform = Scaling.new(0.5, 0.5, 0.5) }
+    w.lights << PointLight.new(Point[-10, 10, -10], Color::WHITE)
   end
 end
