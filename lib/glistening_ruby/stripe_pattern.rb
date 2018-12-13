3927644973827565125d00e9e88084afd2ba3359
Calle Englund <calle@discord.bofh.se>
2018-12-13 22:54:43 +0100

    Render stripey patterns, poorly

--- lib/glistening_ruby/stripe_pattern.rb
+++ lib/glistening_ruby/stripe_pattern.rb
@@ -4,8 +4,8 @@ module GlisteningRuby
   # A striped pattern
   class StripePattern < Base
     def initialize(color_a, color_b)
-      @a = color_a
-      @b = color_b
+      @a = color_a.is_a?(Tuple) ? color_a : Color[*color_a]
+      @b = color_b.is_a?(Tuple) ? color_b : Color[*color_b]
       super
       freeze
     end
