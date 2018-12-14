41f803aeb9ab04b0c8d1d629df280d46f27f03bd
Calle Englund <calle@discord.bofh.se>
2018-12-14 19:45:16 +0100

    Teach patterns to have any number of pigments

--- lib/glistening_ruby/pattern/base.rb
+++ lib/glistening_ruby/pattern/base.rb
@@ -8,14 +8,20 @@ module GlisteningRuby
     # An abstract base class for patterns
     class Base < Transformable
       def initialize(*pigments)
-        @a, @b, = *pigments.map { |p| p.is_a?(Tuple) ? p : Color[*p] }
+        @pigments = pigments.map { |p| p.is_a?(Tuple) ? p : Color[*p] }
+        @pigments << @pigments[0]
+        @a, @b = @pigments[0..1]
         super
       end
 
       attr_reader :a, :b
 
       def color_at(point)
-        @a.interpolate(@b, constrain(grade(point)))
+        g = grade(point)
+        c = @pigments.count - 1
+        p = g.floor % c
+        a, b = @pigments[p..p + 1]
+        a.interpolate(b, constrain(g - p))
       end
 
       def color_at_object(object, point)
