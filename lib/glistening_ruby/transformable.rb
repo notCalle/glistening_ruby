ecba97b4a41d88a079a1b3f10e1aad7db0e0cd7d
Calle Englund <calle@discord.bofh.se>
2018-12-15 11:00:33 +0100

    Teach transformables about points and spaces

--- lib/glistening_ruby/transformable.rb
+++ lib/glistening_ruby/transformable.rb
@@ -19,5 +19,23 @@ module GlisteningRuby
       @inverse = transform.inverse
       @inverse_transpose = transform.submatrix(3, 3).inverse.transpose
     end
+
+    # Transform a point in world space to local space
+    #
+    # :call-seq:
+    #   to_local(world_point) => local_point
+    #
+    def to_local(point)
+      @inverse * point
+    end
+
+    # Transform a point in local space to world space
+    #
+    # :call-seq:
+    #   to_local(local_point) => world_point
+    #
+    def to_world(point)
+      @transform * point
+    end
   end
 end
