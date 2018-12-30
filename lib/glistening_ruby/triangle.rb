db685d07692dcc3e11cb2fbcdeeb2e5774c4becd
Calle Englund <calle@discord.bofh.se>
2018-12-30 21:38:03 +0100

    Teach triangles to have bounding boxes

--- lib/glistening_ruby/triangle.rb
+++ lib/glistening_ruby/triangle.rb
@@ -13,12 +13,21 @@ module GlisteningRuby
       @e1 = @v2 - @v1
       @e2 = @v3 - @v1
       @normal = (@e2.cross @e1).normalize
+      initialize_bounds(@v1, @v2, @v3)
       super
     end
 
-    attr_reader :e1, :e2, :normal, :v1, :v2, :v3
+    attr_reader :bounds, :e1, :e2, :normal, :v1, :v2, :v3
 
     private
+
+    def initialize_bounds(*points)
+      x_min, x_max = points.map(&:x).minmax
+      y_min, y_max = points.map(&:y).minmax
+      z_min, z_max = points.map(&:z).minmax
+      @bounds = [Point[x_min, y_min, z_min],
+                 Point[x_max, y_max, z_max]]
+    end
 
     def object_normal(_point)
       @normal
