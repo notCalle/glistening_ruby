1b573e0e59fa513dd319c18378434df658bf59a3
Calle Englund <calle@discord.bofh.se>
2019-01-02 19:28:52 +0100

    Create DSL include file for the Utah teapot model

---
+++ examples/include/utah_teapot.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'glistening_ruby/dsl'
+
+material :glazed_ceramic do
+  color rgb 1.0, 0.8, 0.5
+  ambient 0.3
+  diffuse 0.7
+  phong 0.9, 300
+  reflective 0.1
+end
+
+shape.mesh :utah_teapot do |version = 'hi'|
+  file "teapot-#{version}.obj"
+  material :glazed_ceramic
+  rotate x: -1/4r
+  scale 1/16r
+end
