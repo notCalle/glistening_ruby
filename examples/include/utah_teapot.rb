10d4253dc8ca281fcb19a61c457253b7a11eab6d
Calle Englund <calle@discord.bofh.se>
2019-01-03 21:20:17 +0100

    Tidy up teapot and cocktails example

--- examples/include/utah_teapot.rb
+++ examples/include/utah_teapot.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-material :glazed_ceramic do
-  color rgb 1.0, 0.8, 0.5
-  ambient 0.3
-  diffuse 0.7
-  phong 0.9, 300
-  reflective 0.1
-end
-
 shape.mesh :utah_teapot do |version = 'hi'|
   file "teapot-#{version}.obj"
-  material :glazed_ceramic
+  material do
+    color rgb 1.0, 0.8, 0.5
+    ambient 0.3
+    diffuse 0.7
+    phong 0.9, 300
+    reflective 0.1
+  end
   rotate x: -1/4r
   scale 1/16r
 end
