fd9ac499ab7aee9dbe7ea0f03f2e0e95b0754fb9
Calle Englund <calle@discord.bofh.se>
2019-01-26 15:43:09 +0100

    Teach spherical lights to have a radius
    Visibility for spherical lights with a radius is calculated by averaging some random samples.

--- lib/glistening_ruby/light/spherical.rb
+++ lib/glistening_ruby/light/spherical.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../vector'
 require_relative 'base'
 
 module GlisteningRuby
@@ -9,6 +10,7 @@ module GlisteningRuby
       def initialize(*)
         super
         @falloff ||= 0.0
+        @radius ||= 0.0
       end
 
       def direction(point)
@@ -19,7 +21,7 @@ module GlisteningRuby
         point_to_light(point).magnitude
       end
 
-      attr_accessor :falloff
+      attr_accessor :falloff, :radius
 
       def intensity(point)
         return @intensity if falloff.zero?
@@ -32,10 +34,27 @@ module GlisteningRuby
         cache[:position] ||= object_to_world Point[0, 0, 0]
       end
 
+      def visibility(point, world, samples)
+        samples = 1 if samples.zero? || @radius.zero?
+        1.upto(samples).map { super }.sum / samples
+      end
+
       private
 
       def point_to_light(point)
         (position - point)
+      end
+
+      def shadow_direction(point)
+        unless @radius.zero?
+          loop do
+            offset = Vector.random
+            next if offset.magnitude > 1.0
+
+            break point += offset * @radius
+          end
+        end
+        point_to_light(point).normalize
       end
     end
   end
